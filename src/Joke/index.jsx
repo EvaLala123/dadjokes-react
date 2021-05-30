c9201788d5cca0d8b57b3bbe8f82977844a87d49
Eva Lastuvkova <eva.lastuvkova@gmail.com>
2021-05-30 12:55:11 +0200

    úkol 14

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import './style.css';
 
 const Joke = () => {
+  const [likeUp, setLikeUp] = useState(0);
+  const [likeDown, setLikeDown] = useState(0);
+
+  const handleLikeUpClick = () => {
+    setLikeUp(likeUp + 1);
+  };
+
+  const handleLikeDownClick = () => {
+    setLikeDown(likeDown + 1);
+  };
+
   return (
     <div className="joke">
       <div className="joke__body">
@@ -20,13 +31,21 @@ const Joke = () => {
       </div>
 
       <div className="joke__likes">
-        <button id="btn-up" className="btn-like btn-like--up"></button>
+        <button
+          id="btn-up"
+          className="btn-like btn-like--up"
+          onClick={handleLikeUpClick}
+        ></button>
         <span id="likes-up" className="likes-count likes-count--up">
-          0
+          {likeUp}
         </span>
-        <button id="btn-down" className="btn-like btn-like--down"></button>
+        <button
+          id="btn-down"
+          className="btn-like btn-like--down"
+          onClick={handleLikeDownClick}
+        ></button>
         <span id="likes-down" className="likes-count likes-count--down">
-          0
+          {likeDown}
         </span>
       </div>
     </div>
